9f04f27c02b919ed747ba2340ec5707bd34ba24e
Nurlia Agustina <liaagustina.app@gmail.com>
2023-05-28 15:03:00 +0800

    menggunakan method get

--- src/praktek3/sepedaAksi.java
+++ src/praktek3/sepedaAksi.java
@@ -8,5 +8,11 @@ public class sepedaAksi {
         s.setharga(1000);
         
         s.cetakinfo();
+        system.out.print("merknya \t: ");
+        system.out.println(s.getmerk());
+        system.out.print("warnanya \t: ");
+        system.out.println(s.getwarna());
+        system.out.print("harganya \t: ");
+        system.out.println(s.getharga());
     }
 }
