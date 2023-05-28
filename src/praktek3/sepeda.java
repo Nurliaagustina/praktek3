fc4dfee3b7d725859e86f7e087f59537c13149a6
Nurlia Agustina <liaagustina.app@gmail.com>
2023-05-28 14:33:20 +0800

    buat method cetak info()

--- src/praktek3/sepeda.java
+++ src/praktek3/sepeda.java
@@ -4,4 +4,11 @@ public class sepeda {
   string merk;
   string warna;
   double harga;
+  
+  void cetakinfo(){
+      system.out.println("merk \t: "+merk+"\n"+
+              "warna \t: "+warna+"\n"+
+              "harga \t  "+harga);
+      
+  }
 }
